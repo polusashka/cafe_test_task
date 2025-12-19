e8c774e192a10ac4e01bf7ac9eb5ffe472baac08
Aleksandr Korzeev <korzeev01@mail.ru>
2025-12-19 17:08:21 +0300

    first_try

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeNegative(t *testing.T) {
@@ -46,5 +48,93 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	type request struct {
+		adr   string
+		count int
+		want  int
+	}
+
+	var requests []request
+
+	testCount := []int{0, 1, 2, 100}
+	cafeTotal := len(cafeList["moscow"])
+
+	for _, v := range testCount {
+		newrequest := request{}
+		newrequest.adr = fmt.Sprintf("/cafe?count=%d&city=moscow", v)
+		newrequest.count = v
+		if v > cafeTotal {
+			newrequest.want = cafeTotal
+			requests = append(requests, newrequest)
+			continue
+		}
+		if v == 0 {
+			newrequest.want = 1
+			requests = append(requests, newrequest)
+			continue
+		}
+		newrequest.want = v
+		requests = append(requests, newrequest)
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.adr, nil)
+
+		handler.ServeHTTP(response, req)
+		body := strings.TrimSpace(response.Body.String())
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		assert.Equal(t, v.want, len(strings.Split(body, ",")))
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	type request struct {
+		adr       string
+		search    string
+		wantCount int
+	}
+
+	testSearch := []string{"фасоль", "кофе", "вилка"}
+	moscowCafes := cafeList["moscow"]
+	var requests []request
+
+	for _, v := range testSearch {
+		newRequest := request{}
+		newRequest.adr = fmt.Sprintf("/cafe?search=%s&city=moscow", v)
+		newRequest.search = v
+		var found []string
+		for _, cafe := range moscowCafes {
+			if strings.Contains(strings.ToLower(cafe), strings.ToLower(v)) {
+				found = append(found, cafe)
+			}
+		}
+		newRequest.wantCount = len(found)
+		requests = append(requests, newRequest)
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.adr, nil)
+
+		handler.ServeHTTP(response, req)
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		resCafes := strings.ToLower(response.Body.String())
+		if v.wantCount != 0 {
+			assert.True(t, strings.Contains(strings.ToLower(resCafes), strings.ToLower(v.search)))
+		}
+		assert.Equal(t, v.wantCount, strings.Count(resCafes, v.search))
 	}
 }
